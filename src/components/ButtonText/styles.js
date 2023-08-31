0d07ce7c4f0cce516b06d113c78697e8c566e6b9
lucas <lucasluz1710@gmail.com>
2023-08-31 13:49:14 -0300

    adding ButtonText and some struture to the Details page

---
+++ src/components/ButtonText/styles.js
@@ -0,0 +1,11 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.button`
+
+  background: none;
+  border: none;
+ 
+  color: ${({ theme, $isActive }) => $isActive ? theme.COLORS.ORANGE : theme.COLORS.GRAY_100};
+  font-weight: 400;
+
+`;
