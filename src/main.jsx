63ad37e39a13b1794992deb369b9a9a160ff46ac
lucas <lucasluz1710@gmail.com>
2023-08-31 11:46:11 -0300

    Added global theme

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+import GlobalStyles from './styles/global';
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+
+    <ThemeProvider theme={theme} >
+      <GlobalStyles />
+    </ThemeProvider>
+
   </React.StrictMode>,
-)
+);
